3b4672ca82e910c730726bed1c6df354bfe45b97
John Young <john@tikaro.net>
2019-12-01 17:31:47 -0500

    First stab at watermarking
    Remove misplaced netlify.toml file
    
    Remove sed script from deploy.sh
    
    Remove the deploy.sh file
    
    Move index.html to the dist folder
    
    Move index.html to the dist folder
    
    Further simplify index.html
    
    First attempt at watermarking using sed
    
    Let's try using an environment variable
    
    Try using process.env.variable
    
    Try using shell variable
    
    Let's try some escaping
    
    Try avoiding a reserved keyword
    
    process.env out. Dollar sign in
    
    try adding both a long and short build number
    
    use bash-style substring truncation
    
    Further replacement attempts
    
    Less cd
    
    Add deploy time
    
    Shorten watermark
    
    Try echo-ing the branch
    
    Try  instead of
    
    Make sure regexp is actually working
    
    Try avoiding another reserved word
    
    Try avoiding another reserved word
    
    Try avoiding another reserved word
    
    Further shorten the watermark

---
+++ scripts/watermark.sh
@@ -0,0 +1,7 @@
+# Copy src/index.html to dist/index.html
+cp src/index.html dist/
+
+# Use sed to watermark dist/index.html
+cd dist
+sed -i "s/\$COMMIT/$(echo $COMMIT_REF | cut -c1-7)/g" index.html
+sed -i "s/\$TIME/$(date)/g" index.html
