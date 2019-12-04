5e5836909cc881ea2d2b170250ab746bb4cdd173
John Young <john@tikaro.net>
2019-12-04 09:21:53 -0500

    Echo out  to see what happens

--- scripts/watermark.sh
+++ scripts/watermark.sh
@@ -3,5 +3,6 @@ cp src/index.html dist/
 
 # Use sed to watermark dist/index.html
 cd dist
+echo "current branch is $BRANCH"
 sed -i "s/\$COMMIT/$(echo $COMMIT_REF | cut -c1-7)/g" index.html
 sed -i "s/\$TIME/$(date)/g" index.html
