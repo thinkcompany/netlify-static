f62853abc89cded7b422e95327ede4ab482593f3
John Young <john@tikaro.net>
2019-12-04 10:16:31 -0500

    Echo out more variables.

--- scripts/watermark.sh
+++ scripts/watermark.sh
@@ -3,6 +3,10 @@ cp src/index.html dist/
 
 # Use sed to watermark dist/index.html
 cd dist
-echo "current branch is $BRANCH"
+echo "BRANCH is $BRANCH"
+echo "HEAD is $HEAD"
+echo "PULL_REQUEST is $PULL_REQUEST"
+echo "REVIEW_ID is $REVIEW_ID"
+
 sed -i "s/\$COMMIT/$(echo $COMMIT_REF | cut -c1-7)/g" index.html
 sed -i "s/\$TIME/$(date)/g" index.html
