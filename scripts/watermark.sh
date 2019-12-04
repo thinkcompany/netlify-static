fb3009782b58972e3f7d5b277b960799e8a7ef48
John Young <john@tikaro.net>
2019-12-04 11:14:47 -0500

    Let's _try_ adding branch name

--- scripts/watermark.sh
+++ scripts/watermark.sh
@@ -1,12 +1,14 @@
 # Copy src/index.html to dist/index.html
 cp src/index.html dist/
 
-# Use sed to watermark dist/index.html
-cd dist
+# Speak environment variables into the deploy log
 echo "BRANCH is $BRANCH"
 echo "HEAD is $HEAD"
 echo "PULL_REQUEST is $PULL_REQUEST"
 echo "REVIEW_ID is $REVIEW_ID"
 
+# Use sed to watermark dist/index.html
+cd dist
+sed -i "s/\$BRANCH/$HEAD/g" index.html
 sed -i "s/\$COMMIT/$(echo $COMMIT_REF | cut -c1-7)/g" index.html
 sed -i "s/\$TIME/$(date)/g" index.html
