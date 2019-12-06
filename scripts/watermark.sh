3a21f559e06c637f20e6ea7ee7326742feae4afc
John Young <john@tikaro.net>
2019-12-05 20:47:04 -0500

    Simplify the watermark if this is not a pull request

--- scripts/watermark.sh
+++ scripts/watermark.sh
@@ -1,18 +1,39 @@
 # Copy src/index.html to dist/index.html
+# In a "real" site, this would be handled by something like Webpack
 cp src/index.html dist/
 
-# Speak environment variables into the deploy log
-echo "------------"
-echo "Netlify environment variables:"
-echo "BRANCH is '$BRANCH'"
-echo "HEAD is '$HEAD'"
-echo "PULL_REQUEST is '$PULL_REQUEST'"
-echo "REVIEW_ID is '$REVIEW_ID'"
+# The Netlify build environment provides environment variables.
+# You can check the deploy log to see what is output from the below:
+echo "-----------------------------"
+echo "Netlify environment variables"
+echo "COMMIT_REF: '$COMMIT_REF'"
+echo "BRANCH: '$BRANCH'"
+echo "HEAD: '$HEAD'"
+echo "PULL_REQUEST: '$PULL_REQUEST'"
+echo "REVIEW_ID: '$REVIEW_ID'"
+echo "-----------------------------"
 
-# Use sed to watermark dist/index.html
+# Add a watermark
+# For pull requests: "Commit abc123 on branch 'feature/new-feature' for PR#456 deployed three days ago'"
+# For a master deploy: "Deployed three days ago"
+
 cd dist
 
+# Truncate long commit hashes to five characters
 sed -i "s/\$COMMIT/$(echo $COMMIT_REF | cut -c1-5)/g" index.html
+
+# Escape forward slashes in branch names.
 sed -i "s/\$BRANCH/$(echo "$HEAD" | sed 's~\/~\\\/~g')/g" index.html
-sed -i "s/\$PR_NUMBER/$REVIEW_ID/g" index.html
+
+# If this is a pull request, add a pull request link.  Otherwise, skip it.
+
+if [ $PULL_REQUEST == 'true' ]
+then
+    sed -i "s/\$PR_NUMBER/$REVIEW_ID/g" index.html
+else
+    sed -i "s/<!-- pr.*//g" index.html
+    sed -i "s/<!-- branch.*//g" index.html
+fi
+
+# Add a date, which timeago.js will display as 'just now' or 'two minutes ago'
 sed -i "s/\$TIME/$(date)/g" index.html
